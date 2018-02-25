8a0dc003e26b6054cc35afa2fc2bc5aca8a03f65
Marek Dziewit <marek2901@gmail.com>
2018-02-25 16:22:02 +0100

    Login and register

--- events_crm/crm/views.py
+++ events_crm/crm/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from django.contrib.auth.decorators import login_required
+
 from .models import Event
 
+
+@login_required
 def index(request):
-    events = Event.objects.all().values('id','name', 'start_date', 'end_date')
+    events = Event.objects.all().values('id', 'name', 'start_date', 'end_date')
     template = loader.get_template('calendar.html')
-    return HttpResponse(template.render({'events' : events}, request))
+    return HttpResponse(template.render({'events': events}, request))
