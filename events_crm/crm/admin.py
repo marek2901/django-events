4f05f72f75f9ae8d5b827eb63877fe62adfbcf17
Marek Dziewit <marek2901@gmail.com>
2018-02-26 18:04:43 +0100

    Extend user attributes by hour rate and skills

--- events_crm/crm/admin.py
+++ events_crm/crm/admin.py
@@ -1,5 +1,40 @@
 from django.contrib import admin
-from crm.models import Event, Equipement
+from crm.models import Event, Equipement, Profile, Skill
+
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 admin.site.register(Event)
 admin.site.register(Equipement)
+
+
+#  Extended user model for Django admin
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+    list_display = ('username', 'email', 'skills', 'hourly_rate')
+
+    def skills(self, instance):
+        return ', '.join(
+            instance.profile.skills.all().values_list('name', flat=True)
+        )
+
+    def hourly_rate(self, instance):
+        return instance.profile.hourly_rate
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Skill)
--- events_crm/crm/models.py
+++ events_crm/crm/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 
 class Equipement(models.Model):
     name = models.CharField(max_length=200)
@@ -29,3 +32,26 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return 'Skill: {}'.format(self.name)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    hourly_rate = models.PositiveIntegerField(blank=True, null=True)
+    skills = models.ManyToManyField(Skill, blank=True)
+
+    def __str__(self):
+        return 'Profil: {}'.format(self.user.username)
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
